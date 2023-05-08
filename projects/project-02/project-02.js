7f50c25586dbeb93d329ea0c3e8a39921165d59b
VanstiW <mw3627@columbia.edu>
2023-05-08 18:32:56 +0800

    5

--- projects/project-02/project-02.js
+++ projects/project-02/project-02.js
@@ -1,117 +1,122 @@
-console.log('this works')
-
-//---------- DATASET
-
-const islands = [
-  {
-    name: "Burton 1995 Kelly Air Camber Snowboard",
-    size: "All Mountain",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/All%20Mountain/A1.jpg?raw=true"
-  },
-  {
-    name: "Burton 1996 Dolphin Camber Snowboard",
-    size: "All Mountain",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/All%20Mountain/A2.jpg?raw=true"
-  },
-  {
-    name: "Burton 2011 Nug Rocker Snowboard",
-    size: "All Mountain",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/All%20Mountain/A3.jpg?raw=true"
-  },
-  {
-    name: "Burton Good Company Camber Snowboard", 
-    size: "Park",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/Park/PA1.jpg?raw=true"
-  },
-  {
-    name: "Burton Blossom Camber Snowboard", 
-    size: "Park",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/Park/PA2.jpg?raw=true"
-  },
-  {
-    name: "Women's Burton Rewind Camber Snowboard", 
-    size: "Park",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/Park/PA3.jpg?raw=true"
-  },
-  {
-    name: "Burton 1995 Kelly Air Camber Snowboard", 
-    size: "Powder",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/Powder/PO1.jpg?raw=true"
-  },
-  {
-    name: "Burton MINE77 x Underberg Family Tree 3D Daily Driver Snowboard", 
-    size: "Powder",
-    image: "https://github.com/VanstiW/GSAPP-CFSP/blob/main/projects/project-02/asset/Powder/PO2.jpg?raw=true"
-  },
-  {
-    name: "Burton Flight Attendant Camber Splitboard", 
-    size: "Splitboards",
-    image: "https://raw.githubusercontent.com/VanstiW/GSAPP-CFSP/main/projects/project-02/asset/Splitboards/S1.jpg"
-  },
-  {
-    name: "Burton Family Tree Straight Chuter Camber Splitboard", 
-    size: "Splitboards",
-    image: "https://raw.githubusercontent.com/VanstiW/GSAPP-CFSP/main/projects/project-02/asset/Splitboards/S2.jpg"
-  },
-
-];
-
-//------------- RENDER ISLANDS TO PAGE
-const ul = document.querySelector("ul");
-
-function renderIslandsToPage(results) {
-  // iterate over data set
-  for(let i = 0; i < results.length; i++){
-    // create the list item
-    let listItem = document.createElement('li');
-    // add a class to each item of the results
-    listItem.classList.add('card', results[i].size) // small
-    // add island name
-    let title = document.createElement('h3')
-    title.textContent = results[i].name // Hainan   
-    // add island size
-    let size = document.createElement('p')
-    size.classList.add(results[i].size)
-    size.textContent = results[i].size
-    // add island image
-    let image = document.createElement('img')
-    image.setAttribute('src', results[i].image)
-
-    ul.appendChild(listItem)
-    listItem.appendChild(title)
-    listItem.appendChild(size)
-    listItem.appendChild(image)
-
-  }
-}
-renderIslandsToPage(islands);
-
-let filterBtns = document.querySelector(".filters");
-let cards = document.querySelectorAll(".card");
-
-function filterFn(event) {
-  console.log(event.target)
-  if(event.target.classList.contains('filter-btn')){
-    // select the current active button
-    let activeBtn = filterBtns.querySelector('.active')
-    activeBtn.classList.remove('active')
-
-    // apply the active class to the target
-    event.target.classList.add('active')
-
-    const filterValue = event.target.getAttribute('data-filter') // yellow | red
-
-    for(let i = 0; i < cards.length; i++){
-      if(cards[i].classList.contains(filterValue) || filterValue === 'all'){
-        cards[i].classList.remove('hide')
-        cards[i].classList.add('show')
-      } else {
-        cards[i].classList.remove('show')
-        cards[i].classList.add('hide')
+// img数据
+let data = [
+    {
+      name: "Burton 1987 Elite Flat Top Snowboard",
+      img: "./image/1.png",
+      hoverImg: "./image/1_1.png",
+      categories: "park",
+    },
+    {
+      name: "Burton 1995 Kelly Air Camber Snowboard",
+      img: "./image/2.png",
+      hoverImg: "./image/2_2.png",
+      categories: "park",
+    },
+    {
+      name: "Burton 1996 Dolphin Camber Snowboard",
+      img: "./image/3.png",
+      hoverImg: "./image/3_3.png",
+      categories: "park",
+    },
+    {
+      name: "Burton 2011 Nug Rocker Snowboard",
+      img: "./image/4.png",
+      hoverImg: "./image/4_4.png",
+      categories: "park",
+    },
+    {
+      name: "Natural Selection Tour x Burton Family Tree Hometown Hero Snowboard",
+      img: "./image/5.png",
+      hoverImg: "./image/5_5.png",
+      categories: "powder",
+    },
+    {
+      name: "Burton MINE77 x Underberg Family Tree 3D Daily Driver Snowboard",
+      img: "./image/6.png",
+      hoverImg: "./image/6_6.png",
+      categories: "powder",
+    },
+    {
+      name: "Burton Family Tree 3D Double Dog Camber Snowboard",
+      img: "./image/7.png",
+      hoverImg: "./image/7_7.png",
+      categories: "powder",
+    },
+    {
+      name: "Women's Burton Hideaway Flat Top Snowboard",
+      img: "./image/8.png",
+      hoverImg: "./image/8_8.png",
+      categories: "powder",
+    },
+    {
+      name: "Men's Burton Ripcord Flat Top Snowboard",
+      img: "./image/9.png",
+      hoverImg: "./image/9_9.png",
+      categories: "splitboards",
+    },
+    {
+      name: "Burton Good Company Camber Snowboard",
+      img: "./image/10.png",
+      hoverImg: "./image/10_10.png",
+      categories: "splitboards",
+    },
+    {
+      name: "Burton Blossom Camber Snowboard",
+      img: "./image/11.png",
+      hoverImg: "./image/11_11.png",
+      categories: "splitboards",
+    },
+    {
+      name: "Men's Burton Process Camber Snowboard",
+      img: "./image/12.png",
+      hoverImg: "./image/12_12.png",
+      categories: "splitboards",
+    },
+  ];
+  const imgContainer = document.getElementById("imgContainer");
+  let currentCategories = [];
+  let html = [];
+  addCategories("park");
+  handleImg();
+  
+  // 处理图片
+  function handleImg() {
+    html = [];
+    for (let i = 0; i < data.length; i++) {
+      if (
+        currentCategories.includes(data[i].categories) ||
+        currentCategories.includes("all")
+      ) {
+        html.push(`<li class="product-image-wrap">
+                <a href="javascript:void(0)">
+                  <img class="img" src="${data[i].img}" alt="" />
+                  <img class="imghover" src="${data[i].hoverImg}" alt="" />
+                </a>
+                <div class="margin-bottom">${data[i].name}</div>
+              </li>`);
       }
-
     }
+    imgContainer.innerHTML = html.join("");
   }
-}
-filterBtns.addEventListener("click", filterFn);
-
+  //  处理选项
+  function addCategories(param) {
+    if (currentCategories.includes(param)) {
+      const index = currentCategories.indexOf(param);
+      if (index > -1) {
+        currentCategories.splice(index, 1);
+      }
+      document
+        .getElementById(param)
+        .querySelector(".checkbox")
+        .classList.remove("active");
+    } else {
+      currentCategories.push(param);
+      document
+        .getElementById(param)
+        .querySelector(".checkbox")
+        .classList.add("active");
+    }
+  
+    handleImg();
+  }
+  
